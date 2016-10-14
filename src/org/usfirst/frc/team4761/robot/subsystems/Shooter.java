653793f4586e2fe9ec96e4bee41807dd4d655a2e
Simon Andrews <simon.andrews212@gmail.com>
2016-10-13 20:30:43 -0400

    Add stub function for rotating

--- src/org/usfirst/frc/team4761/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4761/robot/subsystems/Shooter.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4761.robot.subsystems;
 
+import org.usfirst.frc.team4761.robot.XAxisRelativeDirection;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -17,6 +19,10 @@ public class Shooter extends Subsystem {
     
     public void shoot() {
     	//TODO: Implement shooting
+    }
+    
+    public void rotate(XAxisRelativeDirection direction) {
+    	//TODO: Implement turning (must be very precise)
     }
 }
 

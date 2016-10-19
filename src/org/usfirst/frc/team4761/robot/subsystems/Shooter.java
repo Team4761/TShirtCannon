fcbfe740e6b7a47304fef30ed869f53b779d5672
Jake Backer <jbacker42@gmail.com>
2016-10-18 20:43:46 -0400

    Set up encoder PID

--- src/org/usfirst/frc/team4761/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4761/robot/subsystems/Shooter.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team4761.robot.subsystems;
 
+import org.usfirst.frc.team4761.robot.DummyPIDOutput;
+import org.usfirst.frc.team4761.robot.EncoderPIDSource;
+import org.usfirst.frc.team4761.robot.RobotMap;
 import org.usfirst.frc.team4761.robot.XAxisRelativeDirection;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,6 +18,16 @@ public class Shooter extends Subsystem {
 	
 	public final PIDController controller;
 
+	public Shooter() {
+		controller = new PIDController(0, 0, 0, 
+		new EncoderPIDSource(RobotMap.shooterEncoder, 1, PIDSourceType.kDisplacement), new DummyPIDOutput());
+		
+		controller.disable();
+		controller.setOutputRange(-1.0, 1.0);
+		controller.setPercentTolerance(5.0); // This is subject to change
+		
+	}
+	
     public void initDefaultCommand() {
     	
     }
